74b6e402349b4d3f4436d7dd92784e731526682f
sachin tandon <sachin.tandon@nexla.com>
2025-05-31 13:05:04 -0700

    initialize go module and add main application code
    Initializes Go module and adds main application code
    
    Initialize Go module for the booking application and implement the main functionality to welcome users to the Go conference booking application.

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go conference"
+	fmt.Println("Welcome to " + conferenceName + " booking application")
+	fmt.Println("Get your tickets here to attend")
+}
